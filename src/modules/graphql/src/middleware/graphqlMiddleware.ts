431a6fc096fcd454249daebeb75657d0f464316e
Omicrxn <alexavila0128@gmail.com>
2022-07-25 16:17:10 +0200

    renaming config to schema to fit common sense

--- src/modules/graphql/src/middleware/graphqlMiddleware.ts
+++ src/modules/graphql/src/middleware/graphqlMiddleware.ts
@@ -1,5 +1,5 @@
 import { graphqlHTTP } from "express-graphql";
-import { getSchema } from "../../config/graphQLConfig";
+import { getSchema } from "../../schema/graphQLSchema";
 import { Express } from "express";
 export class GraphQLMiddleware {
   app: Express;
@@ -11,7 +11,7 @@ export class GraphQLMiddleware {
     
     this.app.use(
       "/graphql",
-      await graphqlHTTP({ schema: await getSchema(), graphiql: true })
+      await graphqlHTTP({ schema: await getSchema(), graphiql: false })
     );
   }
 }
